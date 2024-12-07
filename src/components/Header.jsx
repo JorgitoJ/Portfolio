ab8322d1823ec30314a7584c8a2919274a180660
JorgitoJ <jorgeluisvallinas99@gmail.com>
2024-12-07 12:21:51 -0500

    same

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import { RiBookMarkedLine,
 
 export const Header = () => {
   return (
-    <header className='fixed left-0 right-0 top-0 z-50 bg-white/80 backdrop-blur-sm shadow-sm'>
+    <header className='sticky left-0 right-0 top-0 z-50 bg-white/80 backdrop-blur-sm shadow-sm'>
         <nav className='container mx-auto px-6 py-4'>
             <div className='flex justify-between items-center'>
                 <div className='text-2xl font-bold bg-gradient-to-r from-purple-600 to-blue-500 bg-clip-text text-transparent'>
